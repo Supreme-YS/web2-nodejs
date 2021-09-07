05e5651e21003b15daf1bb80298bbe0af1886a1a
Supreme-YS <dudtjr1225@gmail.com>
2021-09-07 19:48:43 +0900

    update directory

--- syntax/Variable.js
+++ syntax/Variable.js
@@ -2,3 +2,7 @@ var a = 1;
 console.log(a);
 a = 2;
 console.log(a);
+
+var name = 'Supreme'
+var letter = 'Lorem ipsum Supreme dolor sit amet,'+name+' consectetur adipisicing Supreme elit, sed do eiusmod tempor incididunt'+name+'ut labore et Supreme dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex Supreme ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, '+name+'sunt in culpa qui officia deserunt mollit anim id est laborum.';
+console.log(letter);
---
+++ syntax/program.js
@@ -0,0 +1,9 @@
+console.log('A');
+console.log('B');
+console.log('C1');
+console.log('D');
+
+console.log('A');
+console.log('B');
+console.log('C2');
+console.log('D');
