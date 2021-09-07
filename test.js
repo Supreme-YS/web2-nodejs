fcf65bf0ffd59b3f0407d5a356986024f23f77be
Supreme-YS <dudtjr1225@gmail.com>
2021-09-07 18:20:20 +0900

    update

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
-console.log("nodejs 테스트용 코드입니다.")
+console.log("nodejs 테스트용 코드입니다. CLI 환경에서 이 프로그램을 실행시키려면")
+console.log("node test.js를 CLI 환경에서 입력합니다.")
 console.log("nodejs 런타임이라는 프로그램이 이 코드들을 읽고 출력해줍니다.")
