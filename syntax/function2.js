c51b9871cdfa4acd53bc762d5ec78f2a27e5b0e8
Supreme-YS <dudtjr1225@gmail.com>
2021-09-08 11:33:10 +0900

    update

---
+++ syntax/function2.js
@@ -0,0 +1,12 @@
+// 반올림을 해주는 메서드
+console.log(Math.round(1.6)); // 2
+console.log(Math.round(1.4)); // 1
+
+// first, second 는 매개변수 (parameter)
+// 전달을 매개해준다. 
+function sum(first, second){
+    console.log(first+second);
+}
+
+// 2, 6은 인자 (argument)
+sum(2, 6); 
