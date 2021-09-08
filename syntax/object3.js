341c6e4bea24b3dcaf36563a65dbdf7f098c1aa0
Supreme-YS <dudtjr1225@gmail.com>
2021-09-08 19:33:13 +0900

    update

---
+++ syntax/object3.js
@@ -0,0 +1,19 @@
+var v1 = 'v1';
+var v2 = 'v2';
+
+/* 정리된 코드 */
+var o = {
+    v1:'v1',
+    v2:'v2'
+}
+
+function f1() {
+    console.log(o.v1);
+}
+
+function f2() {
+    console.log(o.v2);
+}
+
+f1();
+f2();
