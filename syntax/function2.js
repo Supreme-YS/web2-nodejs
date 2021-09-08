d7cb0de6c8f1fde00501dcc80b73e55233e68796
Supreme-YS <dudtjr1225@gmail.com>
2021-09-08 11:38:20 +0900

    javascript function IO

--- syntax/function2.js
+++ syntax/function2.js
@@ -1,4 +1,4 @@
-// 반올림을 해주는 메서드
+// 반올림을 해주는 자바스크립트 내장 함수
 console.log(Math.round(1.6)); // 2
 console.log(Math.round(1.4)); // 1
 
@@ -10,3 +10,13 @@ function sum(first, second){
 
 // 2, 6은 인자 (argument)
 sum(2, 6); 
+
+
+// return
+function sum2(first, second) {
+    console.log('a');
+    return first + second;
+    console.log('b'); // 실행안됨, return을 만나면 함수를 종료시키고 값을 출력한다는 의미
+}
+
+console.log(sum2(2, 6));
