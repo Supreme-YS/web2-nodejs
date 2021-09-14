60363ef7b8ca26dfbf122ccd4a076229b8df47b7
Supreme-YS <dudtjr1225@gmail.com>
2021-09-14 15:32:05 +0900

    update

--- main.js
+++ main.js
@@ -1,35 +1,18 @@
 var http = require('http');
-var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
-var sanitizeHtml = require('sanitize-html');
-var mysql = require('mysql');
-var db = mysql.createConnection({
-  host: 'localhost',
-  user: 'nodejs',
-  password: '111111',
-  database: 'opentutorials'
-});
-db.connect();
+var db = require('./lib/db.js');
+var topic = require('./lib/topic.js');
+
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
   if (pathname === '/') {
     if (queryData.id === undefined) {
-      db.query(`SELECT * FROM topic`, function (error, topics) {
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-        var list = template.list(topics);
-        var html = template.HTML(title, list,
-          `<h2>${title}</h2>${description}`,
-          `<a href="/create">create</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
+      topic.home(request, response);
     } else {
       db.query(`SELECT * FROM topic`, function (error, topics) {
         if (error) {
