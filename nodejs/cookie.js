b8fb5bae703229019ab9e499896ebc93f8c1f096
Supreme-YS <dudtjr1225@gmail.com>
2021-09-29 20:09:26 +0900

    how to write & read cookies

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,6 +1,12 @@
 var http = require('http');
+var cookie = require('cookie');
 http.createServer(function(request, response){
-    
+    console.log(request.headers.cookie);
+    var cookies = {};
+    if (request.headers.cookie !== undefined){
+        cookies = cookie.parse(request.headers.cookie);
+    }
+    console.log(cookies);
     response.writeHead(200, {
         'Set-Cookie' : ['yummy_cookie=choco', 'tasty_cookie=strawberry']
     });
