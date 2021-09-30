4a2dc6941d8b9c4b690595f02a29d804ad8d355b
Supreme-YS <dudtjr1225@gmail.com>
2021-09-30 17:12:20 +0900

    permanent cookies

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -8,14 +8,10 @@ http.createServer(function(request, response){
     }
     console.log(cookies);
     response.writeHead(200, {
-<<<<<<< HEAD
         'Set-Cookie' : [
             'yummy_cookie=choco',
             'tasty_cookie=strawberry', 
             `permanent=cookies; Max-Age=${60*60*24*30}`] // permanant cookie , Max-Age 옵션값을 통해 설정이 가능하다.
-=======
-        'Set-Cookie' : ['yummy_cookie=choco', 'tasty_cookie=strawberry']
->>>>>>> 2b0fae744f0f6c17f8b2af6f9d836694e399e2c6
     });
     response.end("Create Cookie!");
 }).listen(3000);
