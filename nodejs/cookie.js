74cd0eca42d45f42a977e3b2d374a330f8281639
Supreme-YS <dudtjr1225@gmail.com>
2021-09-30 17:28:45 +0900

    Update path, domain

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -13,7 +13,9 @@ http.createServer(function(request, response){
             'tasty_cookie=strawberry', 
             `permanent=cookies; Max-Age=${60*60*24*30}`, // permanant cookie , Max-Age 옵션값을 통해 설정이 가능하다.
             'secure=secure; Secure', // https로만 접근했을 때 쿠키 전송
-            'httponly=httponly; HttpOnly' // 자바스크립트로의 접근을 방지
+            'httponly=httponly; HttpOnly', // 자바스크립트로의 접근을 방지
+            'path=path; Path=/cookie', // 특정 경로에서만 발생하는 쿠키
+            'domain=domain; Domain=o2.org' // 특정 도메인에서만 발생하는 쿠키
         ] 
     });
     response.end("Create Cookie!");
